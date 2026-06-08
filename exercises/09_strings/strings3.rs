8eaad09d2da1e8d1a9ddb027a5bfc8c60503da71
sdowd <sarahlynnedowd@gmail.com>
2026-06-08 16:58:06 -0400

    enums & strings exercise completed

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,17 +1,34 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    let s = " world!";
+    format!("{input}{s}")
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    input.replace("cars", "balloons")
+
 }
 
 fn main() {
     // You can optionally experiment here.
+    let hello: &str = "Здравствуйте";
+    for b in hello.bytes() {
+        println!("{b:b}");
+    }
+
+    for b in hello.bytes() {
+        println!("{b:x}");
+    }
+
+    for c in hello.chars() {
+        println!("{c}");
+    }
 }
 
 #[cfg(test)]
